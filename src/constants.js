21ab8b0431bb32e6243ede0cd3d577e6947c1705
Jon Erickson <jon@deschutesdesigngroup.com>
2023-01-31 13:11:47 -0700

    Add Sentry package and configure init

--- src/constants.js
+++ src/constants.js
@@ -4,6 +4,10 @@ const constants = {
   },
   roster: {
     API_URL: process.env.REACT_APP_API_URL
+  },
+  sentry: {
+    SENTRY_DSN: process.env.REACT_APP_SENTRY_DSN,
+    SENTRY_SAMPLE_RATE: process.env.REACT_APP_SENTRY_SAMPLE_RATE
   }
 }
 
