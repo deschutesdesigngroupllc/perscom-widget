e0a89d685d53de7c6a35ef043ff72fd4d0e4c9af
Jon Erickson <jon@deschutesdesigngroup.com>
2023-01-31 13:12:23 -0700

    Integrate Sentry into project (#11)
    * Add CI/CD flow to trigger a sentry release
    
    * Refractor CI/CD
    
    * Add Sentry package and configure init

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import * as Sentry from '@sentry/react'
+import { BrowserTracing } from '@sentry/tracing'
 import './assets/css/widget.css'
 import Roster from './pages/Roster'
+import { config } from './constants'
+
+Sentry.init({
+  dsn: config.sentry.SENTRY_DSN,
+  integrations: [new BrowserTracing()],
+  tracesSampleRate: config.sentry.SENTRY_SAMPLE_RATE
+})
 
 const rosterDivs = document.querySelectorAll('#perscom_roster')
 rosterDivs.forEach((domElement) => {
