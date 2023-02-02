50fe3e749e2d641a30532806e9d22e9d69fdbf29
Jon Erickson <jon@deschutesdesigngroup.com>
2023-02-01 21:06:36 -0700

    Fixed footer logo bug

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { config } from '../constants'
-import { ReactComponent as Logo } from '../assets/svg/logo.svg'
+import { Logo } from './Logo'
 
 export function Footer() {
   const version = config.version.WIDGET_VERSION
 
   return (
-    <div className='flex items-center mt-2 space-x-2'>
-      <Logo className='w-16' />
+    <div className='flex items-center mt-2 space-x-1'>
+      <Logo className='w-16 h-6' />
       <div id='perscom_footer' className='text-xs text-gray-400'>
         Widget Provided By{' '}
         <a href='https://perscom.io' target='_blank' rel='noreferrer' className='underline'>
