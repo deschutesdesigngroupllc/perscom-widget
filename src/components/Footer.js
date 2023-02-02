ae20c30de451e8eb2dcaa560b563670642ec8151
Jon Erickson <jon@deschutesdesigngroup.com>
2023-02-01 20:29:52 -0700

    Added logo to Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,20 @@
 import React from 'react'
 import { config } from '../constants'
+import { ReactComponent as Logo } from '../assets/svg/logo.svg'
 
 export function Footer() {
   const version = config.version.WIDGET_VERSION
 
   return (
-    <div id='perscom_footer' className='text-xs text-gray-400 mt-4'>
-      Widget Provided By{' '}
-      <a href='https://perscom.io' target='_blank' rel='noreferrer'>
-        PERSCOM.io
-      </a>
-      . Copyright {new Date().getFullYear()} Deschutes Design Group LLC. {version && <span>Version {version}.</span>}
+    <div className='flex items-center mt-2 space-x-2'>
+      <Logo className='w-16' />
+      <div id='perscom_footer' className='text-xs text-gray-400'>
+        Widget Provided By{' '}
+        <a href='https://perscom.io' target='_blank' rel='noreferrer' className='underline'>
+          PERSCOM.io
+        </a>
+        . Copyright {new Date().getFullYear()} Deschutes Design Group LLC. {version && <span>Version {version}.</span>}
+      </div>
     </div>
   )
 }
