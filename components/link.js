09fb0235cf70f4390ef82e527ff7beec647f0115
Jon Erickson <jon@deschutesdesigngroup.com>
2023-12-19 16:38:54 -0800

    Query params now appended to links

--- components/link.js
+++ components/link.js
@@ -1,14 +1,19 @@
+'use client';
+
 import NextLink from 'next/link';
 import cx from 'classnames';
+import { useSearchParams } from 'next/navigation';
 
 export function Link(props) {
+  const params = useSearchParams();
+
   return (
     <NextLink
+      href={{ pathname: props.href, search: params.toString() }}
       className={cx(
         props.className,
         'hover:text-gray-600 active:text-blue-600 dark:hover:text-gray-500 dark:active:text-blue-600'
       )}
-      href={props.href}
     >
       {props.children}
     </NextLink>
