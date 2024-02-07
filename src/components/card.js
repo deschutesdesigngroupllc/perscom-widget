ab3c10d387ac8195c7487ec068677aac636035a6
Jon Erickson <jon@deschutesdesigngroup.com>
2024-02-07 10:04:29 -0800

    Remove widget border and shorten margin (#110)
    * Do not deploy Vercel preview PRs when initiated be dependabot
    
    * Remove card border and make widget margin smaller

--- src/components/card.js
+++ src/components/card.js
@@ -6,7 +6,7 @@ export function Card(props) {
       {...(({ children, ...o }) => o)(props)}
       theme={{
         root: {
-          base: 'flex rounded-lg border border-gray-200 bg-white shadow dark:border-gray-700 dark:bg-gray-800',
+          base: 'flex rounded-lg bg-white shadow dark:border-gray-700 dark:bg-gray-800',
           children: 'flex h-full flex-col justify-center p-0'
         }
       }}
